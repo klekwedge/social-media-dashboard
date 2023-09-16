2e2755a4f43b6547f3f317e8f2d3e484e7ad5eb6
Andrey <antonov.andrey.10101@gmail.com>
2023-09-16 13:04:28 +0300

    feat: add styles for App

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
 <template>
-  <div><Dashboard /></div>
+  <div class="app__wrapper">
+    <main class="main">
+      <Dashboard></Dashboard>
+    </main>
+  </div>
 </template>
 
-<script lang="ts">
+<script>
 import Dashboard from "./components/Dashboard.vue";
 export default {
-  components: {
-    Dashboard,
-  },
-  setup() {
-    return {};
-  },
+  name: "App",
+  components: { Dashboard },
 };
 </script>
 
-<style scoped>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap");
+
+.app {
+  height: 100%;
+  color: var(--textMainColor);
+  background-color: var(--bgColor);
+  &__wrapper {
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+  }
+}
+
+.main {
+  flex: 1 1 auto;
+
+  @media (min-width: 1024px) {
+    position: relative;
+    &::before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 33%;
+      top: 0;
+      left: 0;
+      background-color: var(--bgDecorColor);
+      border-radius: 0 0 35px 35px;
+    }
+  }
+}
 </style>
