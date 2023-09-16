d03e419b4a472c8e40539e8d7db77a7ac96bb82a
Andrey <antonov.andrey.10101@gmail.com>
2023-09-16 12:51:09 +0300

    feat: add TodayCard

---
+++ src/components/TodayCard.vue
@@ -0,0 +1,115 @@
+<template>
+	<div class="today-card">
+		<div class="today-card__content">
+			<div class="today-card__head">
+				<p class="today-card__title">{{ card.title }}</p>
+				<div class="today-card__icon">
+					<img
+						:src="require(`../assets/images/${card.icon}`)"
+						:alt="card.networkName"
+					/>
+				</div>
+			</div>
+			<div class="today-card__body">
+				<div class="today-card__count">
+					{{
+						card.count >= 10000
+							? String(card.count).slice(0, 2) + "k"
+							: card.count
+					}}
+				</div>
+				<div
+					class="today-card__changes"
+					:class="{
+						up: card.status == 'up',
+						down: card.status == 'down'
+					}"
+				>
+					<span>{{ card.changes }}%</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { defineProps } from "vue"
+
+defineProps({
+	card: Object
+})
+</script>
+
+<style lang="scss" scoped>
+.today-card {
+	background-color: var(--bgCardsColor);
+	border-radius: 6px;
+	transition: all 0.3s ease 0s;
+	cursor: pointer;
+	&:hover {
+		background-color: var(--bgCardsColorHover);
+	}
+	&__content {
+		padding: 20px;
+	}
+
+	&__head {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		gap: 15px;
+		margin-bottom: 25px;
+	}
+
+	&__title {
+		font-size: 16px;
+		font-weight: 700;
+		color: var(--textSecondaryColor);
+	}
+
+	&__icon {
+	}
+
+	&__body {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		gap: 15px;
+	}
+
+	&__count {
+		font-size: 34px;
+		font-weight: 700;
+	}
+
+	&__changes {
+		font-size: 14px;
+		font-weight: 700;
+
+		&.up {
+			& span {
+				position: relative;
+				color: var(--limeGreen);
+				&::before {
+					content: url("@/assets/images/icon-up.svg");
+					position: absolute;
+					top: -1px;
+					left: -14px;
+				}
+			}
+		}
+		&.down {
+			& span {
+				position: relative;
+				color: var(--brightRed);
+				&::before {
+					content: url("@/assets/images/icon-down.svg");
+					position: absolute;
+					top: -1px;
+					left: -14px;
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ src/data/data.js
@@ -0,0 +1,102 @@
+export default [
+    {
+        networkName: "facebook",
+        icon: "icon-facebook.svg",
+        nickname: "@nathanf",
+        total: {
+            count: 1987,
+            info: "Followers",
+            status: "up",
+            changes: 12
+        },
+        today: [
+            {
+                title: "Page Views",
+                count: 87,
+                status: "up",
+                changes: 3
+            },
+            {
+                title: "Likes",
+                count: 52,
+                status: "down",
+                changes: 2
+            }
+        ]
+    },
+    {
+        networkName: "twitter",
+        icon: "icon-twitter.svg",
+        nickname: "@nathanf",
+        total: {
+            count: 1044,
+            info: "Followers",
+            status: "up",
+            changes: 99
+        },
+        today: [
+            {
+                title: "Retweets",
+                count: 117,
+                status: "up",
+                changes: 303
+            },
+            {
+                title: "Likes",
+                count: 507,
+                status: "up",
+                changes: 553
+            }
+        ]
+    },
+    {
+        networkName: "instagram",
+        icon: "icon-instagram.svg",
+        nickname: "@realnathanf",
+        total: {
+            count: "11090",
+            info: "Followers",
+            status: "up",
+            changes: "1099"
+        },
+        today: [
+            {
+                title: "Likes",
+                count: 5462,
+                status: "up",
+                changes: 32257
+            },
+            {
+                title: "Profile Views",
+                count: 52000,
+                status: "up",
+                changes: 1375
+            }
+        ]
+    },
+    {
+        networkName: "youtube",
+        icon: "icon-youtube.svg",
+        nickname: "Nathan F.",
+        total: {
+            count: "8239",
+            info: "Subscribers",
+            status: "down",
+            changes: "144"
+        },
+        today: [
+            {
+                title: "Likes",
+                count: 107,
+                status: "down",
+                changes: 19
+            },
+            {
+                title: "Total Views",
+                count: 1407,
+                status: "down",
+                changes: 12
+            }
+        ]
+    }
+]
